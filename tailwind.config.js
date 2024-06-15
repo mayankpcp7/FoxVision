dc1af6144e064c477fec2f5ef66232342a80cb71
mayankpcp7 <jariwalmayank123@gmail.com>
2024-06-15 16:40:34 +0530

    folder

---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [],
+  theme: {
+    extend: {
+      colors: {
+        darkblue: "#071838",
+        sky: "#05D5FB",
+        purple: "#5925F2",
+        lightpurple: "#E6DEFF",
+        lightsky: "#E9FCFF",
+        darkpurple: "#4B17E6",
+        offpurple: " #4B17E60A",
+      },
+    },
+  },
+  plugins: [],
+};
+
