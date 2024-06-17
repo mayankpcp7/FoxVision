1d61497218887e8afbd62455993b5087446468b9
tarunmalik330 <maliktarun2005@gmail.com>
2024-06-17 21:57:39 +0530

    testing in home page

--- src/components/Angebot.jsx
+++ src/components/Angebot.jsx
@@ -1,17 +1,17 @@
 import React from "react";
-import { WhiteHeading } from "./common/Heading";
+import { BlueHeading } from "./common/Heading";
 import { angebotCardList } from "./common/Helper";
 import AngebotCardsList from "./AngebotCardsList";
 import { WhiteLogo } from "./common/Icon";
 
 const Angebot = () => {
   return (
-    <div className="bg-darkpurple xl:pt-[122.94px] xl:pb-[248.65px] md:py-[90px] sm:py-[65px] pt-12 pb-36">
+    <div className="bg-darkpurple xl:pt-[122.94px] xl:pb-[248.65px] md:pt-[90px] md:pb-[190px] sm:pt-[65px] sm:pb-[240px] pt-12 pb-[250px]">
       <div className="container xl:max-w-[1164px] px-3">
         <p className="uppercase text-white text-center font-poppins font-bold sm:text-xl text-sm leading-normal mb-[13.27px]">
           A RESULTS-DRIVEN AGENCY
         </p>
-        <WhiteHeading whiteHeadingText="Angebot" className="text-center" />
+        <BlueHeading headingText="Angebot" className="!text-white text-center" />
         <div className="flex flex-row flex-wrap justify-center xl:gap-[17px] lg:gap-[15px] md:gap-5 sm:gap-3 gap-4 lg:pt-[97.79px] lg:pb-[77.23px] md:py-[60px] sm:py-12 py-9">
           {angebotCardList.map((obj, index) => (
             <div
