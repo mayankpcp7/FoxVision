87bced304b796b75096ef197d41139be690b4b29
tarunmalik330 <maliktarun2005@gmail.com>
2024-06-17 16:35:09 +0530

    complete angebot section and change in common heading

--- src/components/common/Heading.jsx
+++ src/components/common/Heading.jsx
@@ -11,7 +11,7 @@ export const BlueHeading = ({ headingText, className }) => {
 export const WhiteHeading = ({ whiteHeadingText, className }) => {
   return (
     <h2
-      className={`${className} capitalize font-semibold font-montserrat text-6xxl leading-md text-white`}
+      className={`${className} capitalize font-semibold font-montserrat lg:text-6xxl md:text-5xl sm:text-4xl text-3xl leading-md text-white`}
     >
       {whiteHeadingText}
     </h2>
