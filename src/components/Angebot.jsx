61c82eb288ab188ec4b95771b19f7a4fc5393692
tarunmalik330 <maliktarun2005@gmail.com>
2024-06-18 15:14:18 +0530

    change in cards

--- src/components/Angebot.jsx
+++ src/components/Angebot.jsx
@@ -8,7 +8,7 @@ import { SubHeading } from "./common/SubHeading";
 const Angebot = () => {
   return (
     <div className="bg-darkpurple xl:pt-[122.94px] lg:pt-24 md:pt-[80px] sm:pt-14 pt-12 xl:pb-[248.65px] md:pb-[190px] sm:pb-[240px] pb-[250px]">
-      <div className="max-w-[1164px] px-3">
+      <div className="max-w-[1164px] mx-auto px-3">
         <SubHeading
           className="text-white mb-[13.27px] text-center"
           text="A RESULTS-DRIVEN AGENCY"
