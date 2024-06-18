f9699827dcab12ff1f224685af34f7a130ffd43f
tarunmalik330 <maliktarun2005@gmail.com>
2024-06-18 10:36:26 +0530

    start design mastery section

---
+++ src/components/FriendlyAndReliable.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import { SubHeading } from "./common/SubHeading";
+
+const FriendlyAndReliable = () => {
+  return (
+    <div className="container xl:max-w-[1164px] mx-auto px-3">
+          <SubHeading text="FOX VISION - Friendly and reliable" className="" />
+          <h1>fox-vision</h1>
+    </div>
+  );
+};
+
+export default FriendlyAndReliable;
--- src/pages/AboutUs.jsx
+++ src/pages/AboutUs.jsx
@@ -2,12 +2,14 @@ import React from "react";
 import Home from "./Home";
 import DesignProjectAgency from "../components/DesignProjectAgency";
 import HowWeWork from "../components/common/HowWeWork";
+import FriendlyAndReliable from "../components/FriendlyAndReliable";
 
 const AboutUs = () => {
   return (
     <>
       <Home />
       <DesignProjectAgency />
+      <FriendlyAndReliable />
       <HowWeWork />
     </>
   );
