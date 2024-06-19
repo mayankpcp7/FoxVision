18ea0edadde445d8312df7af5dc56fa076095f2e
tarunmalik330 <maliktarun2005@gmail.com>
2024-06-19 11:07:33 +0530

    remove hero in offer page

--- src/pages/Offer.jsx
+++ src/pages/Offer.jsx
@@ -3,12 +3,10 @@ import Design from "../components/Design";
 import DesignStrategy from "../components/DesignStrategy";
 import Frequently from "../components/Frequently";
 import CorporateIdentity from "../components/CorporateIdentity";
-import Hero from "../components/common/Hero";
 
 const Offer = () => {
   return (
     <div className="overflow-hidden">
-      <Hero />
       <CorporateIdentity />
       <Design />
       <DesignStrategy />
