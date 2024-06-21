8bd5148982523eb65f93be20a23d2d5451c3214e
mayankpcp7 <jariwalmayank123@gmail.com>
2024-06-21 11:48:05 +0530

    merge code

--- src/components/Conversation.jsx
+++ src/components/Conversation.jsx
@@ -55,7 +55,7 @@ const Conversation = () => {
     // Update errors state
     setFormErrors(errors);
 
-    // If no errors, submit the form (you can handle submission logic here)
+    // If no errors, submit the form
     if (Object.keys(errors).length === 0) {
       console.log("Form data:", formData);
       // Reset form
@@ -68,13 +68,14 @@ const Conversation = () => {
         message: "",
         robotCheck: false,
       });
+      // Optionally, you can perform further submission logic here
     }
   };
 
   return (
     <>
       <div className="max-w-[1164px] px-3 mx-auto">
-        <div className="flex items-center sm:pt-16 pt-12 md:pt-20 lg:pt-24 xl:mt-32 md:flex-row flex-col gap-8">
+        <div className="flex items-center sm:pt-16 pt-12 md:pt-20 lg:pt-24 xl:pt-0 xl:mt-32 md:flex-row flex-col gap-8">
           <div className="flex-col flex">
             <SubHeading
               text="Contact With FoxVision"
@@ -91,10 +92,14 @@ const Conversation = () => {
               alt="phone"
             />
           </div>
-          <div className="flex flex-col md:items-start items-center md:ms-12">
-            <form className="w-full" onSubmit={handleSubmit} action="#">
-              <div className="flex gap-3">
-                <div>
+          <div className="flex flex-col w-full md:items-start items-center md:ms-12">
+            <form
+              className="w-full md:mx-0 mx-auto"
+              onSubmit={handleSubmit}
+              action="#"
+            >
+              <div className="flex w-full justify-center md:justify-start gap-3">
+                <div className="w-full max-w-[297px]">
                   <input
                     className={`max-w-[297px] w-full outline-none px-2 h-[55px] border shadow-forminput border-offgrey ${
                       formErrors.firstName ? "border-red-500" : ""
@@ -111,7 +116,7 @@ const Conversation = () => {
                     </p>
                   )}
                 </div>
-                <div>
+                <div className="max-w-[297px] w-full">
                   <input
                     className={`max-w-[297px] w-full outline-none px-2 h-[55px] border shadow-forminput border-offgrey ${
                       formErrors.lastName ? "border-red-500" : ""
@@ -129,8 +134,8 @@ const Conversation = () => {
                   )}
                 </div>
               </div>
-              <div className="flex gap-3 mt-3">
-                <div>
+              <div className="flex w-full justify-center md:justify-start gap-3 mt-3">
+                <div className="w-full max-w-[297px]">
                   <input
                     className={`max-w-[297px] w-full outline-none px-2 h-[55px] border shadow-forminput border-offgrey ${
                       formErrors.subject ? "border-red-500" : ""
@@ -145,7 +150,7 @@ const Conversation = () => {
                     <p className="text-red-500 text-sm">{formErrors.subject}</p>
                   )}
                 </div>
-                <div>
+                <div className="w-full max-w-[297px]">
                   <input
                     className={`max-w-[297px] w-full outline-none px-2 h-[55px] border shadow-forminput border-offgrey ${
                       formErrors.mobile ? "border-red-500" : ""
@@ -161,68 +166,68 @@ const Conversation = () => {
                   )}
                 </div>
               </div>
-              <input
-                className={`w-full outline-none max-w-[605px] mt-3 px-2 h-[55px] border shadow-forminput border-offgrey ${
-                  formErrors.email ? "border-red-500" : ""
-                }`}
-                type="email"
-                name="email"
-                value={formData.email}
-                onChange={handleChange}
-                placeholder="Email"
-              />
-              {formErrors.email && (
-                <p className="text-red-500 text-sm">{formErrors.email}</p>
-              )}
-              <textarea
-                className={`max-w-[605px] outline-none resize-none py-4 px-2 w-full h-[159px] mt-3 border shadow-forminput border-offgrey ${
-                  formErrors.message ? "border-red-500" : ""
-                }`}
-                name="message"
-                value={formData.message}
-                onChange={handleChange}
-                
-                placeholder="Tell us more about your projects"
-              ></textarea>
-              {formErrors.message && (
-                <p className="text-red-500 text-sm">{formErrors.message}</p>
-              )}
-              <div className="flex w-full max-w-[302px] mt-3 border items-center justify-between bg-inputgrey py-2 px-3 border-lightgrey rounded-md">
-                <div>
-                  {" "}
-                  <label htmlFor="robotCheck" className="flex items-center">
-                    <input
-                      type="checkbox"
-                      id="robotCheck"
-                      name="robotCheck"
-                      checked={formData.robotCheck}
-                      onChange={handleChange}
+              <div className="flex flex-col justify-center md:justify-start items-center md:items-start">
+                <input
+                  className={`w-full mx-auto outline-none max-w-[605px] mt-3 px-2 h-[55px] border shadow-forminput border-offgrey ${
+                    formErrors.email ? "border-red-500" : ""
+                  }`}
+                  type="email"
+                  name="email"
+                  value={formData.email}
+                  onChange={handleChange}
+                  placeholder="Email"
+                />
+                {formErrors.email && (
+                  <p className="text-red-500 text-sm">{formErrors.email}</p>
+                )}
+                <textarea
+                  className={`max-w-[605px] outline-none resize-none py-4 px-2 w-full h-[159px] mt-3 border shadow-forminput border-offgrey ${
+                    formErrors.message ? "border-red-500" : ""
+                  }`}
+                  name="message"
+                  value={formData.message}
+                  onChange={handleChange}
+                  placeholder="Tell us more about your projects"
+                ></textarea>
+                {formErrors.message && (
+                  <p className="text-red-500 text-sm">{formErrors.message}</p>
+                )}
+                <div className="flex w-full justify-center md:justify-start max-w-[302px] mt-3 border items-center justify-between bg-inputgrey py-2 px-3 border-lightgrey rounded-md">
+                  <div>
+                    <label htmlFor="robotCheck" className="flex items-center">
+                      <input
+                        type="checkbox"
+                        id="robotCheck"
+                        name="robotCheck"
+                        checked={formData.robotCheck}
+                        onChange={handleChange}
+                      />
+                      <p className="font-poppins font-normal ms-2 text-base text-robotgrey">
+                        I am not a robot
+                      </p>
+                    </label>
+                    {formErrors.robotCheck && (
+                      <p className="text-red-500 text-sm">
+                        {formErrors.robotCheck}
+                      </p>
+                    )}
+                  </div>
+                  <div className="flex flex-col items-center">
+                    <img
+                      className="max-w-[49px]"
+                      src={recaptcha}
+                      alt="recaptcha"
                     />
-                    <p className="font-poppins font-normal ms-2 text-base text-robotgrey">
-                      I am not a robot
+                    <p className="font-poppins font-normal text-xxsm text-privacy">
+                      Privacy - Terms
                     </p>
-                  </label>
-                  {formErrors.robotCheck && (
-                    <p className="text-red-500 text-sm">
-                      {formErrors.robotCheck}
-                    </p>
-                  )}
+                  </div>
                 </div>
-                <div className="flex flex-col items-center">
-                  <img
-                    className="max-w-[49px]"
-                    src={recaptcha}
-                    alt="recaptcha"
-                  />
-                  <p className="font-poppins font-normal text-xxsm text-privacy">
-                    Privacy - Terms
-                  </p>
-                </div>
+                <Button
+                  className="max-w-[302px] w-full mt-4"
+                  btnName="Send Message"
+                />
               </div>
-              <Button
-                className="max-w-[302px] w-full mt-4"
-                btnName="Send Message"
-              />
             </form>
           </div>
         </div>
--- src/components/FriendlyAndReliable.jsx
+++ src/components/FriendlyAndReliable.jsx
@@ -4,36 +4,38 @@ import mhillpaloka from "../assets/images/webp/project-agency/mhill-paloka.webp"
 
 const FriendlyAndReliable = () => {
   return (
-    <div className="max-w-[1164px] mx-auto px-3 lg:pb-[6px]">
-      <SubHeading
-        text="FOX VISION - Friendly and reliable"
-        className="text-center lg:!mb-3 !mb-1 !normal-case"
-      />
-      <h2 className="capitalize font-semibold font-montserrat xl:text-6xxl md:text-5xl sm:text-4xl text-3xl leading-md text-darkblue text-center lg:mb-[55px] sm:mb-12 mb-8 max-sm:px-1">
-        Design Mastery <span className="lowercase">in</span> Our Genes
-      </h2>
-      <div className="flex flex-row flex-wrap justify-center">
-        <div className="lg:p-9 sm:p-7 p-4 shadow-phil overflow-hidden flex lg:flex-row max-lg:max-w-[500px] flex-col-reverse flex-wrap items-center">
-          <div className="lg:w-6/12 lg:pt-0 pt-8">
-            <p className="capitalize text-darkblue font-poppins font-semibold sm:text-2xl text-xl !leading-normal sm:mb-[15px] mb-3">
-              Mhill Paloka
-            </p>
-            <p className="text-darkblue opacity-70 font-poppins font-normal xl:text-xl sm:text-base text-sm !leading-lg xl:max-w-[500px] lg:max-w-[430px]">
-              Mhill Paloka deepened his know-how in the areas of purchasing,
-              marketing and controlling in a large international company. He has
-              also been a founding member of a facility management company since
-              2009. Design has always been extremely important to him and he is
-              happy to help you stay up to date.
-            </p>
-          </div>
-          <div className="lg:w-6/12 w-full">
-            <img
-              width={530}
-              height={392}
-              src={mhillpaloka}
-              alt="mhillpaloka"
-              className="rounded-[6px] w-full"
-            />
+    <div className="px-3">
+      <div className="max-w-[1164px] overflow-hidden mx-auto ">
+        <SubHeading
+          text="FOX VISION - Friendly and reliable"
+          className="text-center lg:!mb-3 !mb-1 !normal-case"
+        />
+        <h2 className="capitalize font-semibold font-montserrat xl:text-6xxl md:text-5xl sm:text-4xl text-3xl leading-md text-darkblue text-center lg:mb-[55px] sm:mb-12 mb-8 max-sm:px-1">
+          Design Mastery <span className="lowercase">in</span> Our Genes
+        </h2>
+        <div className="flex flex-row flex-wrap justify-center">
+          <div className="lg:p-9 sm:p-7 p-4 shadow-phil overflow-hidden flex lg:flex-row max-lg:max-w-[500px] flex-col-reverse flex-wrap items-center">
+            <div className="lg:w-6/12 lg:pt-0 pt-8">
+              <p className="capitalize text-darkblue font-poppins font-semibold sm:text-2xl text-xl !leading-normal sm:mb-[15px] mb-3">
+                Mhill Paloka
+              </p>
+              <p className="text-darkblue opacity-70 font-poppins font-normal xl:text-xl sm:text-base text-sm !leading-lg xl:max-w-[500px] lg:max-w-[430px]">
+                Mhill Paloka deepened his know-how in the areas of purchasing,
+                marketing and controlling in a large international company. He
+                has also been a founding member of a facility management company
+                since 2009. Design has always been extremely important to him
+                and he is happy to help you stay up to date.
+              </p>
+            </div>
+            <div className="lg:w-6/12 w-full">
+              <img
+                width={530}
+                height={392}
+                src={mhillpaloka}
+                alt="mhillpaloka"
+                className="rounded-[6px] w-full"
+              />
+            </div>
           </div>
         </div>
       </div>
